660fa1aa93a1350dbe5ff6b94306fceff0040410
ericy <ericy@stud.ntnu.no>
2019-03-11 13:01:58 +0100

    Login and signup changes, added functionality to buttons

--- src/sample/Controller/signUpController.java
+++ src/sample/Controller/signUpController.java
@@ -1,8 +1,15 @@
 package sample.Controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,11 +21,41 @@ public class signUpController {
     private URL location;
 
     @FXML
+    private JFXPasswordField confirmPasswordInput;
+
+    @FXML
+    private JFXTextField usernameInput;
+
+    @FXML
+    private JFXButton goBackButton;
+
+    @FXML
     private JFXButton signUpButton;
 
     @FXML
+    private JFXPasswordField passwordInput;
+
+    @FXML
+    private JFXTextField emailInput;
+
+    @FXML
     void initialize() {
+        goBackButton.setOnAction(event -> {
+            goBackButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/View/login.fxml"));
+            try{
+                loader.load();
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+
+        });
 
     }
-
 }
