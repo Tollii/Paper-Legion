bf2a8f1aba5e884818e8f52a86441fb11efe2dae
ericy <ericy@stud.ntnu.no>
2019-03-13 15:45:45 +0100

    Almost there

--- src/dragAndDrop/Main.java
+++ src/dragAndDrop/Main.java
@@ -28,8 +28,12 @@ public class Main extends Application {
     private static final int tileSize = 100; //
     static int selectedPosX; //Holder position X til den brikken som er markert.
     static int selectedPosY; //Holder position Y til den brikken som er markert.
+    static int draggedTilePosX;
+    static int draggedTilePosY;
     static boolean selected=false;
     GridPane ins = new GridPane(); // For alle tiles.
+    static Grid testGrid = new Grid(6,6); //Sets up Grid 6x6
+    private MouseEvent event9;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -37,9 +41,9 @@ public class Main extends Application {
         Scene scene1;                //Scene for second and third phase of the game
         StackPane sp = new StackPane();
         sp.setAlignment(Pos.BASELINE_LEFT); //Only baseline_Left is correct according to positions.
-        Grid testGrid = new Grid(6,6); //Sets up Grid 6x6
+
         ins.getChildren().add(testGrid.gp); //Insert grid from Grid class.
-        scene1 = new Scene(sp, 500,400);
+        scene1 = new Scene(sp, 800,600);
 
         //Bruker denne som spiller brikke foreløpig.
         //Piece tile = new Piece(100,100, 0,0,100,false);
@@ -56,6 +60,7 @@ public class Main extends Application {
                 for(int j=0; j<piecesListe[i].length; j++){
                     if(piecesListe[i][j]!= null){
                         piecesListe[i][j].setStroke(Color.TRANSPARENT);
+                        //testGrid.liste[i][j].setFill(Color.TRANSPARENT);
                     }
                 }
             }
@@ -80,26 +85,32 @@ public class Main extends Application {
         //Velger hvilken brikke du vil bruke.
         scene1.addEventHandler(MouseEvent.MOUSE_CLICKED, event2 -> {
             int posX = getPosXFromEvent(event2);
-            int posY = getPosYromEvent(event2);
+            int posY = getPosYFromEvent(event2);
 
-            if(piecesListe[posY][posX] != null) {
-                if (!selected) {
-                    piecesListe[posY][posX].setOldPos(piecesListe[posY][posX].getTranslateX()/100, piecesListe[posY][posX].getTranslateY()/100);
-                    piecesListe[posY][posX].setStrokeType(StrokeType.INSIDE);
-                    piecesListe[posY][posX].setStrokeWidth(3);
-                    piecesListe[posY][posX].setStroke(Color.RED);
-                    selected = true;
-                    selectedPosX = posX;
-                    selectedPosY = posY;
+            if(piecesListe[posY][posX] != piecesListe[selectedPosY][selectedPosX]){
+                if(piecesListe[posY][posX] != null) {
+                    if (!selected) {
+                        piecesListe[posY][posX].setOldPos(piecesListe[posY][posX].getTranslateX()/100, piecesListe[posY][posX].getTranslateY()/100);
+                        piecesListe[posY][posX].setStrokeType(StrokeType.INSIDE);
+                        piecesListe[posY][posX].setStrokeWidth(3);
+                        piecesListe[posY][posX].setStroke(Color.RED);
+                        selected = true;
+                        selectedPosX = posX;
+                        selectedPosY = posY;
+                        //highlightPossibleMoves(selectedPosX, selectedPosY);
+                        event2.consume();
+                    }
                 }
+                ifDragged(posX, posY);
             }
         });
+
 
         //Når du har valgt en brikke, så kan du angripe en annen med denne.
         sp.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             if(selected){
                 int nyPosX = getPosXFromEvent(event);
-                int nyPosY = getPosYromEvent(event);
+                int nyPosY = getPosYFromEvent(event);
                 if (piecesListe[nyPosY][nyPosX] != null) {
                     if (piecesListe[nyPosY][nyPosX].getEnemy()) {
                         if(piecesListe[nyPosY][nyPosX] != piecesListe[selectedPosY][selectedPosX]){
@@ -113,8 +124,7 @@ public class Main extends Application {
                                         event = null;
                                         // må og fjernes fra eventuelle lister denne fienden kan ligge i.
                                     }
-
-                                    }
+                                }
                             }
                         }
                     }
@@ -126,61 +136,132 @@ public class Main extends Application {
 
 
         ////////////////////////////////////////////////////////BEVEGELSE//////////////////////////////////////////////////////////////
-//        sp.addEventHandler(MouseEvent.MOUSE_CLICKED,event2 -> {
-//            double rectPosX1 = tileSize;
-//            double rectPosY1 = tileSize;
-//            double posX1 = event2.getSceneX();
-//            double posY1 =event2.getSceneY();
-//            double movementX1 = posX1-rectPosX1;
-//            double movementY1 = posY1-rectPosY1;
-//            double a1 = (int) (Math.ceil(movementX1/100.0)); // Runder til nærmeste 100 for snap to grid funksjonalitet
-//            double b1 = (int) (Math.ceil(movementY1/100.0)); // Runder til nærmeste 100 for snap to grid funksjonalitet
+
+
+
+
+//            piecesListe[][].addEventHandler(MouseEvent.MOUSE_RELEASED, e ->{
+//                //Hvis avstanden er 2 eller mindre
+//                if(!(Math.abs(a/100-piecesListe[(int)b1][(int)a1].getOldPosX())>2) && (!(Math.abs(b/100-piecesListe[(int)b1][(int)a1].getOldPosY())>2))){
 //
+//                        if(piecesListe[][] == null){
+//                            piecesListe[][].setTranslateX(a);
+//                            piecesListe[][].setTranslateY(b);
 //
-//            piecesListe[(int)b1][(int)a1].addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
-//                double rectPosX = piecesListe[(int)b1][(int)a1].getLayoutX() +tileSize;
-//                double rectPosY = piecesListe[(int)b1][(int)a1].getLayoutY() +tileSize;
-//                double precRectPosX = tileSize/2;
-//                double precRectPosY = tileSize/2;
-//                double posX = event.getSceneX();
-//                double posY =event.getSceneY();
-//                double movementX = posX-rectPosX;
-//                double movementY = posY-rectPosY;
-//                double movementPrecX = posX-precRectPosX;
-//                double movementPrecY = posY-precRectPosY;
-//
-//                double a = (int) (Math.ceil(movementX/100.0))*100; // Runder til nærmeste 100 for snap to grid funksjonalitet
-//                double b = (int) (Math.ceil(movementY/100.0))*100; // Runder til nærmeste 100 for snap to grid funksjonalitet
-//
-//                piecesListe[(int)b1][(int)a1].setTranslateX(movementPrecX);
-//                piecesListe[(int)b1][(int)a1].setTranslateY(movementPrecY);
-//
-//                piecesListe[(int)b1][(int)a1].addEventHandler(MouseEvent.MOUSE_RELEASED, e ->{
-//                    //Hvis avstanden er 2 eller mindre
-//                    if(!(Math.abs(a/100-oldPosX/100)>2) && (!(Math.abs(b/100-oldPosY/100)>2))){
-//                        if(a/100 >=0 && a/100<testGrid.getColumns() && b/100 >=0 && b/100<testGrid.getRows()){
-//                            if(piecesListe[(int) b/100][(int)a/100] == null){
-//                                piecesListe[(int)b1][(int)a1].setTranslateX(a);
-//                                piecesListe[(int)b1][(int)a1].setTranslateY(b);
-//
-//                                piecesListe[(int)b][(int)a] = piecesListe[(int)b1][(int)a1];
-//                                piecesListe[(int)b1][(int)a1]= null;
-//                            }
+//                            piecesListe[][] = piecesListe[][];
+//                            piecesListe[][] = null;
 //                        }
-//                        //Hvis avstanden er stører enn 2, flyttes ikke brikken.
-//                    } else{
-//                        piecesListe[(int)b1][(int)a1].setTranslateX(piecesListe[(int)b1][(int)a1].getOldPosX());
-//                        piecesListe[(int)b1][(int)a1].setTranslateY(piecesListe[(int)b1][(int)a1].getOldPosY());
 //                    }
-//                });
+//                    //Hvis avstanden er stører enn 2, flyttes ikke brikken.
+//                } else{
+//                    piecesListe[][].setTranslateX(piecesListe[][].getOldPosX());
+//                    piecesListe[][].setTranslateY(piecesListe[][].getOldPosY());
+//                }
 //            });
-//        });
+
+
+
+
+
+
+
         /////////////////////////////////////////BEVEGELSE SLUTT///////////////////////////////////////////////////////
 
 
         window.setTitle("Hello World");
         window.setScene(scene1);
         window.show();
+    }
+
+    private void ifDragged(int startPosX, int startPosY) {
+        if(selected) {
+            piecesListe[startPosY][startPosX].setOldPos(startPosX, startPosY);
+
+            piecesListe[startPosY][startPosX].addEventHandler(MouseEvent.MOUSE_DRAGGED, event1 -> {
+              double precPosX = getPrecPosXFromEvent(event1);
+              double precPosY = getPrecPosYFromEvent(event1);
+              piecesListe[startPosY][startPosX].setTranslateX(precPosX);
+              piecesListe[startPosY][startPosX].setTranslateY(precPosY);
+
+
+            });
+
+            piecesListe[startPosY][startPosX].addEventHandler(MouseEvent.MOUSE_RELEASED, event2 -> {
+                int dropPosX = getPosXFromEvent(event2);
+                int dropPosY = getPosYFromEvent(event2);
+                if(piecesListe[dropPosY][dropPosX] == null) {
+                    if(withinBounds(dropPosX, dropPosY)){
+                        if (dropPosX >= 0 && dropPosX < testGrid.getColumns() && dropPosY >= 0 && dropPosY < testGrid.getRows()) {
+                            piecesListe[startPosY][startPosX].setTranslateX(dropPosX * 100);
+                            piecesListe[startPosY][startPosX].setTranslateY(dropPosY * 100);
+
+                            piecesListe[dropPosY][dropPosX] = piecesListe[startPosY][startPosX];
+////                                selectedPosX = dropPosX;
+////                                selectedPosY = dropPosY;
+
+
+                            piecesListe[selectedPosY][selectedPosX] = null;
+                            selectedPosX = dropPosX;
+                            selectedPosY = dropPosY;
+
+                            for(int i=0; i<piecesListe.length; i++){
+                                for(int j=0; j<piecesListe[i].length; j++){
+                                    System.out.println(piecesListe[i][j]);
+                                }
+                            }
+                        } else {
+                            piecesListe[startPosY][startPosX].setTranslateX(piecesListe[startPosY][startPosX].getOldPosX() * 100);
+                            piecesListe[startPosY][startPosX].setTranslateY(piecesListe[startPosY][startPosX].getOldPosY() * 100);
+
+                        }
+                    }
+
+
+                }
+            });
+        }
+
+    }
+
+    private void highlightPossibleMoves(int posX, int posY) {
+
+//        System.out.println("PosX+1: " +(posX+1));
+//        System.out.println("PosX-1: " +(posX-1));
+//        System.out.println("PosY+1: " +(posY+1));
+//        System.out.println("PosY-1: " +(posY-1));
+//
+//        if((posX-1)>=0 && (posX+1)>=0 && (posX-1)<=6 && (posX+1<=6)) {
+//            //UP DOWN LEFT RIGHT
+//            testGrid.liste[posY][posX - 1].setFill(Color.DARKRED);
+//            testGrid.liste[posY][posX + 1].setFill(Color.DARKRED);
+//            testGrid.liste[posY - 1][posX].setFill(Color.DARKRED);
+//            testGrid.liste[posY + 1][posX].setFill(Color.DARKRED);
+//
+//
+//            //Corners
+//            testGrid.liste[posY + 1][posX + 1].setFill(Color.DARKRED);
+//            testGrid.liste[posY - 1][posX - 1].setFill(Color.DARKRED);
+//            testGrid.liste[posY + 1][posX - 1].setFill(Color.DARKRED);
+//            testGrid.liste[posY - 1][posX + 1].setFill(Color.DARKRED);
+//
+//        }
+
+
+        }
+
+
+    private void clearHighlightPossible(int posX, int posY) {
+//        //UP DOWN LEFT RIGHT
+//        testGrid.liste[posY][posX-1].setFill(Color.TRANSPARENT);
+//        testGrid.liste[posY][posX+1].setFill(Color.TRANSPARENT);
+//        testGrid.liste[posY-1][posX].setFill(Color.TRANSPARENT);
+//        testGrid.liste[posY+1][posX].setFill(Color.TRANSPARENT);
+//
+//        //Corners
+//        testGrid.liste[posY+1][posX+1].setFill(Color.TRANSPARENT);
+//        testGrid.liste[posY-1][posX-1].setFill(Color.TRANSPARENT);
+//        testGrid.liste[posY+1][posX-1].setFill(Color.TRANSPARENT);
+//        testGrid.liste[posY-1][posX+1].setFill(Color.TRANSPARENT);
     }
 
     private boolean withinBounds(int nyPosX, int nyPosY) {
@@ -190,6 +271,18 @@ public class Main extends Application {
         return false;
     }
 
+    private double getPrecPosXFromEvent(MouseEvent event2) {
+        double rectPosX1 = tileSize/2;
+        double posX1 = event2.getSceneX();
+        return (posX1-rectPosX1);
+    }
+
+    private double getPrecPosYFromEvent(MouseEvent event2) {
+        double rectPosY1 = tileSize/2;
+        double posY1 = event2.getSceneY();
+        return (posY1-rectPosY1);
+    }
+
     private int getPosXFromEvent(MouseEvent event2) {
         double rectPosX1 = tileSize;
         double posX1 = event2.getSceneX();
@@ -197,7 +290,7 @@ public class Main extends Application {
         return (int) (Math.ceil(movementX1/100.0)); // Runder til nærmeste 100 for snap to grid funksjonalitet
     }
 
-    private int getPosYromEvent(MouseEvent event2) {
+    private int getPosYFromEvent(MouseEvent event2) {
         double rectPosY1 = tileSize;
         double posY1 =event2.getSceneY();
         double movementY1 = posY1-rectPosY1;
