8a709861f19ca5836e2fee695a2005ab1223417e
Joakim Adolfsenm <Joakim.adolfsen@gmail.com>
2019-03-14 13:52:01 +0100

    Still testing code and making adjustments

--- src/mysql/Signin.sql
+++ src/mysql/Signin.sql
@@ -8,4 +8,6 @@ SELECT hashedpassword,passwordsalt FROM Users WHERE username = 'admin';
 -- Register new user.
 INSERT INTO Users (username,hashedpassword,passwordsalt,email,online_status) VALUES (?,?,?,?,0);
 
-
+-- Update Online Status
+UPDATE TABLE Users SET online_status = 1 WHERE username = ?;
+UPDATE TABLE Users SET online_status = 1 WHERE username = ?;
