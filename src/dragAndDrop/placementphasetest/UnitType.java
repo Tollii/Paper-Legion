d1bce88dc47cfb5f3e496dbe2895b9aa75bfb0e8
krisvane <krisvane@stud.ntnu.no>
2019-03-18 21:00:43 +0100

    added interface unittype

---
+++ src/dragAndDrop/placementphasetest/UnitType.java
@@ -0,0 +1,26 @@
+package dragAndDrop;
+
+public interface UnitType {
+  final double hp, dmg, atkRange, movement;
+  final String type;
+
+  static public String getType() {
+    return type;
+  }
+
+  static public double getHp() {
+    return hp;
+  }
+
+  static public double getDmg() {
+    return dmg;
+  }
+
+  static public double getAtkRange() {
+    return atkRange;
+  }
+
+  static public double getMovement() {
+    return movement;
+  }
+}
