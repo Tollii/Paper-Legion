56febd93685609b5f997565499a7c3d537d2b41a
krisvane <krisvane@stud.ntnu.no>
2019-03-19 15:54:07 +0100

    la til metoder i tile klassene

--- src/dragAndDrop/Tile.java
+++ src/dragAndDrop/Tile.java
@@ -9,11 +9,28 @@ import javafx.scene.layout.*;
 
 public class Tile extends StackPane { //hver rute er en stackpane omkapslet rundt et rektangel
 
+  Unit unit;
+
     public Tile(int sizeX, int sizeY){
       Rectangle rect = new Rectangle(sizeX, sizeY, Color.WHITE);
       rect.setStrokeType(StrokeType.INSIDE);
       rect.setStroke(Color.BLACK);
 
       this.getChildren().add(rect);
+    }
+
+    //get og set metoder
+    public void setUnit(Piece unit) {
+      this.unit = unit;
+      this.getChildren().add(unit);
+    }
+
+    public Unit getUnit() {
+      return unit;
+    }
+
+    //metode for å fjerne unit når den flyttes
+    public void removeUnit() {
+      this.getChildren().remove(unit);
     }
 }
--- src/dragAndDrop/placementphasetest/UnitReferenceTile.java
+++ src/dragAndDrop/placementphasetest/UnitReferenceTile.java
@@ -9,11 +9,22 @@ import javafx.scene.layout.*;
 
 public class UnitReferenceTile extends StackPane{ //hver rute er en stackpane omkapslet rundt et rektangel
 
+  Unit unit;
+
   public UnitReferenceTile(int sizeX, int sizeY) {
     Rectangle rect = new Rectangle(sizeX, sizeY, Color.WHITE);
     rect.setStrokeType(StrokeType.INSIDE);
     rect.setStroke(Color.BLACK);
 
     this.getChildren().add(rect);
+  }
+
+  public void setUnit(Piece unit) {
+    this.unit = unit;
+    this.getChildren().add(unit);
+  }
+
+  public Unit getUnit() { //muligens unødvendig
+    return unit;
   }
 }
