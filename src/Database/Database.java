07194ea71c55ce2186be595ec10511bbd56d8693
thomabmo <thomabmo@stud.ntnu.no>
2019-03-19 13:32:37 +0100

    Fikset imports i Databaseklassen etter merge

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,8 +1,12 @@
 package Database;
 
 import dragAndDrop.ProtoUnitType;
-import dragAndDrop.UnitType;
-
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
