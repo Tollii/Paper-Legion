01e378755b83109348f0502b3cad7ed90512b922
jonbergq <jon.bergquist3@gmail.com>
2019-03-19 11:23:36 +0100

    La inn Junit test på login og endret noen få ting

--- src/hashAndSalt/testing/SignUpTest.java
+++ src/hashAndSalt/testing/SignUpTest.java
@@ -1,4 +1,47 @@
 package hashAndSalt.testing;
 
-public class SignUpTest  {
+import hashAndSalt.SignUp;
+import hashAndSalt.Login;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class LoginTest {
+    private SignUp instance;
+    private SignUp loginInstance;
+
+
+    public LoginTest() {
+
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        //TODO legg inn at man sletter det som ble laget i testen.
+    }
+
+    @Before
+    public void setUp() {
+        //lager nytt objekt for hver test
+        instance = new Login();
+    }
+
+    @After
+    public void tearDown() {
+        instance.logout();
+    }
+
+// TODO catch sql unique hentes exception, test at det som lages kan logges inn i med login. Lag en User.
 }
+
+
