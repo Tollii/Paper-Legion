889bb08b1f9ba4cb32a4118e828db10adb92b1ca
krisvane <krisvane@stud.ntnu.no>
2019-03-21 03:02:18 +0100

    added dragboard and som other small changes tile classes

--- src/dragAndDrop/Tile.java
+++ src/dragAndDrop/Tile.java
@@ -6,10 +6,17 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.scene.layout.*;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
 
 public class Tile extends StackPane { //hver rute er en stackpane omkapslet rundt et rektangel
 
-  Unit unit;
+  private Unit unit;
 
     public Tile(int sizeX, int sizeY){
       Rectangle rect = new Rectangle(sizeX, sizeY, Color.WHITE);
@@ -17,10 +24,32 @@ public class Tile extends StackPane { //hver rute er en stackpane omkapslet rund
       rect.setStroke(Color.BLACK);
 
       this.getChildren().add(rect);
+
+      //lagger merke til om en dragevent skjer over tilen, og klargjør tilen for en overføring i form av kopiering
+      this.setOnDragOver(event -> {
+        Dragboard db = event.getDragboard();
+
+        if (event.getGestureSource() != this && db.hasImage() && db.hasString()) {
+          event.acceptTransferModes(TransferMode.COPY);
+        }
+      });
+
+      //event på når drageventen droppes over tilen
+      this.setOnDragDropped(event -> {
+        Dragboard db = event.getDragboard();
+
+        boolean success = false;
+        if (db.hasImage() && db.hasString()) {
+          Unit newUnit = newUnit(false, db.getString()); //skaper ny unit og legger den på tilen
+          setUnit(newUnit);
+          success = true;
+        }
+        event.setDropCompleted(success);
+      });
     }
 
     //get og set metoder
-    public void setUnit(Piece unit) {
+    public void setUnit(Unit unit) {
       this.unit = unit;
       this.getChildren().add(unit);
     }
@@ -31,6 +60,7 @@ public class Tile extends StackPane { //hver rute er en stackpane omkapslet rund
 
     //metode for å fjerne unit når den flyttes
     public void removeUnit() {
+      unit = null;
       this.getChildren().remove(unit);
     }
 }
