97c0c3c544307dbf0fa87a71956904c8ad7dc10a
jonbergq <jon.bergquist3@gmail.com>
2019-04-02 15:08:39 +0200

    la inn at testBrukererne lager og joiner game i setUpClass(), de funker ikke pga gamelogic

--- src/tests/GameLogicTest.java
+++ src/tests/GameLogicTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import java.sql.ResultSet;
 import database.Cleaner;
 import database.ConnectionPool;
 import database.Database;
@@ -15,10 +16,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import static database.Variables.*;
 import static org.junit.Assert.*;
 
-//testUser2 Password: testPassword2
-//testUser password: testPassword
+//testUser2 Password: testPassword2 id: 51
+//testUser password: testPassword id: 3
 
 public class GameLogicTest {
 
@@ -31,8 +33,32 @@ public class GameLogicTest {
         setUp.importUnitTypes();
         GameLogic game = new GameLogic();
 
-        database.Variables.user_id = 3;
+        user_id = 3;
         Variables.opponent_id = 2;
+        db.createGame(user_id);
+
+        ConnectionPool connectionPool;
+        connectionPool = ConnectionPool.create();
+        Connection myConn = connectionPool.getConnection();
+        ResultSet rs = null;
+        String stmt = "SELECT match_id FROM Matches WHERE game_started=0 AND player1 = 3; ";
+        PreparedStatement preparedQuery = null;
+        try {
+            preparedQuery = myConn.prepareStatement(stmt);
+            rs = preparedQuery.executeQuery();
+            rs.next();
+            match_id = rs.getInt("match_id");
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            Cleaner.closeStatement(preparedQuery);
+            connectionPool.releaseConnection(myConn);
+        }
+
+
+        db.joinGame(match_id, opponent_id);
     }
 
     @AfterClass
