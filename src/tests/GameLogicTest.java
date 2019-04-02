e4bca5b097c8122541ab7afab4b7298b17954114
joakimad <joakim.adolfsen@gmail.com>
2019-04-02 14:15:46 +0200

    Added test class

---
+++ src/tests/GameLogicTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import database.Cleaner;
+import database.ConnectionPool;
+import database.Database;
+import org.junit.*;
+import org.junit.Test;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class GameLogicTest {
+
+    @Test
+
+    @BeforeClass
+    public static void setUpClass() throws SQLException {
+
+    }
+
+    @AfterClass
+    public static void tearDownClass()  {
+
+    }
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
