f2f73d05ce5b83ce67c1f716ae2808c6669fe947
ericy <ericy@stud.ntnu.no>
2019-04-05 18:00:15 +0200

    finished with movement info, template for rest is inserted, but not working properly

--- src/menus/Controller/GameInfoController.java
+++ src/menus/Controller/GameInfoController.java
@@ -1,9 +1,12 @@
 package menus.Controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,6 +28,35 @@ public class GameInfoController extends Controller {
     @FXML
     private ImageView paperLegionLogo;
 
+    @FXML
+    private ImageView movementImage;
+
+    @FXML
+    private HBox movementHbox;
+
+
+    @FXML
+    private Pane movementPane;
+
+    @FXML
+    private JFXTextArea movementText;
+
+
+    @FXML
+    private Pane backgroundImagePane;
+
+
+    @FXML
+    private JFXTextArea attackText;
+
+    @FXML
+    private JFXTextArea placementText;
+
+    @FXML
+    private Pane attackbackgroundImagePane;
+
+    @FXML
+    private Pane placementbackgroundImagePane;
 
 
     @FXML
@@ -34,6 +66,23 @@ public class GameInfoController extends Controller {
             changeScene("mainMenu.fxml");
         });
 
+        backgroundImagePane.setStyle("-fx-background-image: url(/menus/menuAssets/movementInfo.png);" +
+                "-fx-background-size: cover;" +
+                "-fx-background-position: top left;");
+
+        attackbackgroundImagePane.setStyle("-fx-background-image: url(/menus/menuAssets/attackInfo.png);" +
+                "-fx-background-size: cover;" +
+                "-fx-background-position: top left;");
+
+        placementbackgroundImagePane.setStyle("-fx-background-image: url(/menus/menuAssets/placement.png);" +
+                "-fx-background-size: cover;" +
+                "-fx-background-position: top left;");
+
+
+
+
+
+        //IMAGES USE 13:9 RATIO
 
     }
 
