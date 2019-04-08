9b1596fa65ffe01d0e2c732feac759de7814b265
ericy <ericy@stud.ntnu.no>
2019-04-08 15:29:06 +0200

    Finished Javadoc for Cleaner class

--- src/database/Cleaner.java
+++ src/database/Cleaner.java
@@ -6,10 +6,15 @@ import java.sql.*;
  * A simple class that cleans up after SQL statements.
  *
  */
-
-
 public class Cleaner {
 
+    /**
+     * Closes ResultSets, usually this method is called upon
+     * in a finally block
+     * @param res Takes in a ResultSet.
+     * @see ResultSet
+     * @throws SQLException Throws SQLEXCEPTION if it fails.
+     */
     public static void closeResSet(ResultSet res){
         try{
             if(res != null && !res.isClosed()){
@@ -20,6 +25,13 @@ public class Cleaner {
         }
     }
 
+    /**
+     * Closes statements, usually this method is called upon
+     * in a finally block
+     * @param st Takes in a Statement
+     * @see Statement
+     * @throws SQLException Throws SQLEXCEPTION if it fails.
+     */
     public static void closeStatement(Statement st){
         try{
             if(st != null && !st.isClosed()){
@@ -30,6 +42,13 @@ public class Cleaner {
         }
     }
 
+    /**
+     * Closes Connection, usually this method is called upon
+     * in a finally block
+     * @param con Takes in a Connection
+     * @see Connection
+     * @throws SQLException Throws SQLEXCEPTION if it fails.
+     */
     public static void closeConnection(Connection con){
         try{
             if(con != null && !con.isClosed()){
@@ -40,6 +59,13 @@ public class Cleaner {
         }
     }
 
+    /**
+     * Rollback changes if error occurs, this method has to be called upon
+     * in a finally block.
+     * @param con Takes in a Connection
+     * @see Connection
+     * @throws SQLException Throws SQLEXCEPTION if it fails.
+     */
     public static void rollBack(Connection con){
         try{
             if(con != null && !con.getAutoCommit()){
@@ -50,6 +76,13 @@ public class Cleaner {
         }
     }
 
+    /**
+     * Sets auto commit = true for given connection, this method can to be called upon
+     * in a finally block.
+     * @param con Takes in a Connection
+     * @see Connection
+     * @throws SQLException Throws SQLEXCEPTION if it fails.
+     */
     public static void setAutoCommit(Connection con){
         try{
             if(con != null && !con.getAutoCommit()){
